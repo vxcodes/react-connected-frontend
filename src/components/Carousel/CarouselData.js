63c3a4b299ee9adc3ce8c8616bccb4ca2f1cb880
Viraj Patel <pnviraj@gmail.com>
2021-06-29 23:57:01 -0700

    wip: changes banner imgs

--- src/components/Carousel/CarouselData.js
+++ src/components/Carousel/CarouselData.js
@@ -1,14 +1,11 @@
 export const CarouselData = [
   {
-    image:
-      'https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlJTIwY29ubmVjdGluZ3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    image: 'banner1.png',
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1495837174058-628aafc7d610?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlJTIwY29ubmVjdGluZ3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    image: 'banner2.png',
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
+    image: 'banner3.png',
   },
 ];
